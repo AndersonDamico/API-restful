d969d6f8e126e556d2eb34d57e92112a2700dab9
AndersonDamico <andersondamico2@gmail.com>
2022-10-10 17:57:22 -0300

    adicionando metodo get

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,7 +8,7 @@ put- atualizar dados e DELETE para remover dados */
 
 const ProductController = require ('../controllers/products')
 
-router.get('/products', ProductController.get )
+router.get('/products/:id?', ProductController.get )
 /*router.post('/products', ProductController.post)
 router.put('/products/:id', ProductController.put)
 router.delete('/products/:id', ProductController.delete)*/
